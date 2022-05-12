deeac50bce92ef1eb8e17cdad4a3a7b926ff57b9
krox2 <developer-krox@yandex.ru>
2022-05-12 12:21:39 +0300

    120522-1220 Создан проект с pug

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <Header></Header>
+    <div class="wrapper-content">
+        <div class="container">
+           <router-view></router-view>
+        </div>
+      </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import {contentHome} from "@/_config";
+
+export default {
+    components: { Header, Footer},
+  data() {
+    return {
+      content: contentHome
+    }
+  },
+  }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer" >
+    <div class="container">
+        <ul class="navbar-list">
+          <li class="navbar-item" v-for="link in links" :key="link.alias" >
+            <router-link class="navbar-link" :to="link.url">{{ link.title }}</router-link>
+          </li>
+        </ul>
+      </div>
+  </footer>
+</template>
+
+<script>
+import {listNavMenu} from "@/_config"
+export default {
+  name: 'Footer-m',
+  data() {
+    return {
+      links: listNavMenu,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
